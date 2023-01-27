5b80de5e82239b7a4d3dc48800b07e6c01212318
Marvin Emechebe <emechebeonyebuchi@gmail.com>
2023-01-27 09:07:58 +0200

    removed logo

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Version Number
-
-5b8d0fd276b6d288905ed2f63a934e057e8feca2
-
 ### `yarn start`
 
 Runs the app in the development mode.
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Logo from "./logo.svg";
 import { Calculator } from "./components";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <img src={Logo} alt="logo" className={"App-logo"} />
       <Calculator />
     </div>
   );
